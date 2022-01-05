802e057fb9ea5b76efa6586add1fb5b0ea731d80
M.Tamura <tamura2004@gmail.com>
2022-01-05 21:29:11 +0900

    first commit

---
+++ src/App.svelte
@@ -0,0 +1,46 @@
+<script>
+  import Alignment from "./Alignment.svelte";
+  import Classes from "./Classes.svelte";
+
+  let pc = {
+    alignment: null,
+    klasses: [],
+  };
+</script>
+
+<main>
+  <div class="container">
+    <div class="row">
+      <div class="col-6">
+        <table class="table table-striped">
+          <tbody>
+            <tr>
+              <th>属性</th>
+              <td>{pc.alignment}</td>
+            </tr>
+            {#each pc.klasses as klass, i}
+              <tr>
+                <th>lv{i + 1}</th>
+                <td>{klass}</td>
+              </tr>
+            {/each}
+          </tbody>
+        </table>
+      </div>
+      <div class="col-6">
+        {#if !pc.alignment}
+          <p>アラインメントを選択してください</p>
+          <Alignment bind:alignment={pc.alignment} />
+        {:else}
+          <p on:click={() => (pc.alignment = null)}>
+            アラインメント：{pc.alignment}
+          </p>
+          <Classes {pc} bind:klasses={pc.klasses} />
+        {/if}
+      </div>
+    </div>
+  </div>
+</main>
+
+<style>
+</style>
---
+++ src/Classes.svelte
@@ -0,0 +1,43 @@
+<script>
+  export let pc = null;
+  export let klasses = [];
+
+  let classes = [
+    ["アルケミスト", () => true],
+    ["バーバリアン", () => !pc.alignment.includes("秩序")],
+    ["バード", () => true],
+    ["ドルイド", () => pc.alignment.includes("中立")],
+    ["ファイター", () => true],
+    ["インクィジター", () => true],
+    ["キネティスト", () => true],
+    ["メイガス", () => true],
+    ["モンク", () => pc.alignment.includes("秩序")],
+    ["パラディン", () => pc.alignment === "秩序にして善"],
+    ["レンジャー", () => true],
+    ["ローグ", () => true],
+    ["スレイヤー", () => true],
+    ["ソーサラー", () => true],
+    ["ウィザード", () => true],
+  ];
+
+  const select = (c) => () => (klasses = [...klasses, c]);
+</script>
+
+<div class="panel">
+  {#each classes as [c, cond]}
+    {#if cond()}
+      <button class="btn btn-outline-dark" on:click={select(c)}>
+        {c}
+      </button>
+    {/if}
+  {/each}
+</div>
+
+<style>
+  .panel {
+    display: grid;
+    grid-template-columns: repeat(3, 6em);
+    grid-template-rows: repeat(20, 6em);
+    grid-gap: 1em;
+  }
+</style>
